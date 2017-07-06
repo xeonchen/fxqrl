2adc9bb1d7e0e89464edf781ccd60fbc93ecab50
Liang-Heng Chen <xeonchen@gmail.com>
2017-07-06 22:32:52 +0800

    fix issue #3, add settings UI and cellSize setting

--- popup/fxqrl.js
+++ popup/fxqrl.js
@@ -1,7 +1,8 @@
-'use strict';
+"use strict";
 
 const [ QR_VERSION_MIN, QR_VERSION_MAX ] = [ 1, 40 ];
 const DEFAULT_ERROR_CORRECTION_LEVEL = 'L'; // 'L', 'M', 'Q', 'H'
+const [ DEFAULT_CELL_SIZE, DEFAULT_MARGIN_SIZE] = [ 4, 8 ];
 
 function generateQR(text, cellSize=1, margin=0) {
   for (let i = QR_VERSION_MIN; i <= QR_VERSION_MAX; ++i) {
@@ -18,8 +19,14 @@ function generateQR(text, cellSize=1, margin=0) {
 }
 
 browser.tabs.query({active: true, currentWindow: true}).then(tabs => {
-  let tag = generateQR(tabs[0].url, 4, 0);
-  let doc = new DOMParser().parseFromString(tag, 'text/html');
-  let img_el = doc.getElementsByTagName('img')[0];
-  document.getElementById('qrcode_img').src = img_el.src;
+
+  browser.storage.local.get('cellSize').then(result => {
+    let tag = generateQR(tabs[0].url,
+                         parseInt(result.cellSize) || DEFAULT_CELL_SIZE,
+                         DEFAULT_MARGIN_SIZE);
+    let doc = new DOMParser().parseFromString(tag, 'text/html');
+    let img_el = doc.getElementsByTagName('img')[0];
+    document.getElementById('qrcode_img').src = img_el.src;
+  });
+
 });
